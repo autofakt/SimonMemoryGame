3fd8bb1a5b8bd5e0f57917279d39480d82a8f382
MB <autofakt5@gmail.com>
2021-05-24 20:55:35 -0700

    adding new features and sequence.

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var gamePattern = [];
 var userClickedPattern = [];
 var gameLive = false;
 var level = 0;
+var userTurnNum = 0;
 
 
 $(document).keypress(function(){
@@ -14,13 +15,29 @@ $(document).keypress(function(){
 
 
 function nextSequence(){
+  userTurnNum = 0;
 var randomNumber = Math.floor(Math.random()*4);
 var randomChosenColour = buttonColours[randomNumber];
 console.log(randomChosenColour);
 gamePattern.push(randomChosenColour);
 $("#level-title").text("Level "+ level++);
-$("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-playSound(randomChosenColour);
+playSequence();
+//$("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+//playSound(randomChosenColour);
+}
+
+function playSequence(){
+
+  for(var i = 0; i<gamePattern.length; i++){
+        playHelper(i);
+  }
+}
+
+function playHelper(i){
+  setTimeout(function () {
+    $("#"+gamePattern[i]).fadeOut(100).fadeIn(100);
+    playSound(gamePattern[i]);
+  }, 500 *i);
 }
 
 function buttonClicked(){
@@ -29,7 +46,28 @@ function buttonClicked(){
   playSound(userChosenColor);
   animatePress(userChosenColor);
   console.log(userClickedPattern);
+  checkAnswer();
+  if (userTurnNum==gamePattern.length){
+    userTurnNum =1;
+    nextSequence();
+  }
 }
+
+function checkAnswer(){
+  for(var i = 0; i<gamePattern.length; i++){
+    console.log("Game Pattern");
+    console.log(gamePattern[i]);
+    if(gamePattern[i]==userClickedPattern[i]){
+      console.log("match");
+    } else {
+      alert("Wrong");
+      return;
+    }
+    userTurnNum++;
+    console.log("User turn #: " +  userTurnNum);
+  }
+}
+
 
 function playSound(name){
   var audio = new Audio("sounds/"+name+".mp3");
